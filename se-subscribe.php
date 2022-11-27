6f669a3988286f0701aa4e1ffffc28017ec27bbf
jabber <2898117012@qq.com>
2022-11-28 02:27:51 +0800

    init

---
+++ se-subscribe.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: SE Subscribe
+ * Plugin URI: https://wordpress.org/plugins/se-subscribe
+ * Description: Subscribe Form
+ * Author: Shopeo
+ * Version: 0.0.1
+ * Author URI: https://shopeo.cn
+ * License: GPL2+
+ * Text Domain: se-subscribe
+ * Domain Path: /languages
+ * Requires at least: 5.9
+ * Requires PHP: 5.6
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
